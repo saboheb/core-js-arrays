2d4065a26a50fe1248ff01c0d9a50959f6cadff0
Alex Stolbov <100lbov21@gmail.com>
2025-11-11 09:06:05 +0400

    decide task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,11 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from(
+    { length: end - start + 1 },
+    (value, index) => start + index
+  );
 }
 
 /**
@@ -37,8 +40,15 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const lengthNewArr = Math.max(arr1.length, arr2.length);
+  const resultArr = new Array(lengthNewArr).fill(0);
+
+  arr1.forEach((val, i) => { resultArr[i] += val; });
+  arr2.forEach((val, i) => { resultArr[i] += val; });
+
+
+  return resultArr;
 }
 
 /**
