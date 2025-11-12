2707caa5aae9b4a27148e41fd0d045631608ee06
Alex Stolbov <100lbov21@gmail.com>
2025-11-12 09:55:42 +0400

    feat: decide isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -142,8 +142,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const len = arr[0].length;
+  return arr.every((str) => str.length === len);
 }
 
 /**
